7810833f84a6e384061ae15c0adeb26d77ba9db0
julienneknowles <julienneknowles@gmail.com>
2024-06-09 17:38:32 -0500

    Added CSS styling, appended content and username input onto blog

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,8 +1,42 @@
-
-cont themeSwitcher = document.querySelector('#theme-switcher');
+const body = document.querySelector('body');
+const blogContainer = document.querySelector('main');
+const themeSwitcher = document.querySelector('#theme-switcher');
 const container = document.querySelector('.container');
+
+
 
 let mode = 'light';
 
-themeSwitcher.addEventListener('click, function() {
-    ')
+//themeSwitcher.addEventListener('click, function() {
+  //  ');
+
+//pulling input data from local storage into new blog page
+ const storedEntries = JSON.parse(localStorage.getItem('blogposts'));
+
+ console.log(storedEntries);
+
+ for (let i=0; i < storedEntries.length; i++) {
+ 
+   // entries[i].entry
+    //entries[i].title
+    //entries[i].content 
+
+ const article= document.createElement('article');
+ const postSubject = document.createElement('h2');
+ const userName = document.createElement('h3');
+ const content = document.createElement('p');
+ 
+ postSubject.textContent= `${storedEntries[i].title}`;
+ article.appendChild(postSubject);
+ blogContainer.appendChild(article);
+
+ userName.textContent= `${storedEntries[i].entry}`; 
+ article.appendChild(userName);
+ blogContainer.appendChild(article);
+
+content.textContent= `${storedEntries[i].content}`;
+article.appendChild(content);
+blogContainer.appendChild(article);
+
+ }
+
--- Assets/js/form.js
+++ Assets/js/form.js
@@ -3,14 +3,38 @@ const inputUsername = document.querySelector('#username');
 const inputTitle = document.querySelector('#title');
 const inputContent = document.querySelector('#content');
 const submitButton = document.querySelector('#submit');
+const feedback = document.querySelector('#feedback');
+
+if (localStorage.getItem('blogposts') !==null) {
+    entries = JSON.parse(localStorage.getItem('blogposts'));
+}  else { entries = [];}
 
 submitButton.addEventListener('click', function(event) {
     event.preventDefault();
-    getPost();
-    console.log(entries);
-});
 
-let entries = [];
+let entries;
+//throw error when input values are empty
+if (inputUsername.value === '') {
+ displayMessage('error', 'Username cannot be blank');
+} else if (inputTitle.value === '') {
+    displayMessage('error', 'Title cannot be blank');
+} else if (inputContent.value === '') {
+    displayMessage('error', 'Content cannot be blank');
+}  else { getPost();
+    console.log(entries);
+    displayMessage('success', 'Post Submitted');
+
+    window.location.href="blog.html";
+
+}});
+
+function displayMessage (type, message) {
+    feedback.textContent = message;
+}
+
+
+
+//let entries = [];
 function getPost() {
 
 const post = {
@@ -21,4 +45,6 @@ content: inputContent.value,
 entries.push(post)
 localStorage.setItem('blogposts', JSON.stringify(entries));
 }
+
+
 
